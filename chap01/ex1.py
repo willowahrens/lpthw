3bb6b9e8bab201fb13b64b04eeee1177e59973ec
Willow Ahrens <willowahrens@Willows-MacBook-Pro.local>
2017-06-21 14:41:07 -0500

    exercise 2

--- chap01/ex1.py
+++ chap01/ex1.py
@@ -1,1 +1,19 @@
-print('one line')
+print "hello world!"
+print "hello again!"
+print "i like typing this!"
+print "this is fun!"
+print "yay! printing!"
+print "i'd much rather you 'not'"
+print "i 'said' do not touch this"
+print "make your script print another line"
+
+
+# chap01(master)[ 2 ] > python ex1.py
+# hello world!
+# hello again!
+# i like typing this!
+# this is fun!
+# yay! printing!
+# i'd much rather you 'not'
+# i 'said' do not touch this
+# make your script print another line
---
+++ chap02/ex2.py
@@ -0,0 +1,7 @@
+# A comment, this is so you can read your program later.
+# Anything after # is ignored by python.
+
+print "I could have code like this." # and the comment after is ignored
+# print "this won't run"
+
+print "this will run"
