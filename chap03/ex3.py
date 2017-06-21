1a8ee29db3a2ff26dc8211830f345a68758f9ec6
Willow Ahrens <willowahrens@Willows-MacBook-Pro.local>
2017-06-21 15:07:50 -0500

    example

--- chap03/ex3.py
+++ chap03/ex3.py
@@ -1,7 +1,10 @@
+#prints
 print "i will now count my chickens"
-
+## floating number
 print "hens", 25 + 30 / 6
+#floating number
 print "roosters", 100 - 25 * 3 % 4
+#prints
 print "now i will count my eggs"
 
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
@@ -21,3 +24,19 @@ print "how about some more"
 print "is it greater?", 5 > -2
 print "is it greater or equal?", 5 >= -2
 print "is it less or equal?", 5 <= -2
+
+# chap03(master)[ 3 ] > python ex3.py
+# i will now count my chickens
+# hens 30
+# roosters 97
+# now i will count my eggs
+# 7
+# is it true that 3 + 2 < 5 - 7?
+# False
+# what is 3 + 2? 5
+# what is 5 - 7? -2
+# oh, that's why it's false
+# how about some more
+# is it greater? True
+# is it greater or equal? True
+# is it less or equal? False
