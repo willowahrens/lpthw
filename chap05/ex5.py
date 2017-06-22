9a87447eac03d204034eaf2327e2bae165c88d3b
Willow Ahrens <willowahrens@Willows-MacBook-Pro.local>
2017-06-22 13:58:22 -0500

    example

---
+++ chap05/ex5.py
@@ -0,0 +1,27 @@
+my_name = 'Willow Ahrens'
+my_age = 17
+my_height = 60
+my_weight = 134
+my_eyes = 'green'
+my_teeth = 'white'
+my_hair = 'blonde'
+
+print "let's talk about %s" % my_name
+print "she's %d inches tall" % my_height
+print "she's %d pounds heavy" % my_weight
+print "actually that's not too heavy"
+print "she's got %s eyes and %s hair" % (my_eyes, my_hair)
+print "her teeth are usually %s depending on the coffee" % my_teeth
+
+
+print "if i add %d, %d, and %d I get %d" % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+
+
+# chap05(master)[ 1 ] > python ex5.py
+# let's talk about Willow Ahrens
+# she's 60 inches tall
+# she's 134 pounds heavy
+# actually that's not too heavy
+# she's got green eyes and blonde hair
+# her teeth are usually white depending on the coffee
+# if i add 17, 60, and 134 I get 211
