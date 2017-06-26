19ff926d6918d769acda152adf772201f161f38f
Willow Ahrens <willowahrens@Willows-MacBook-Pro.local>
2017-06-26 17:21:37 -0500

    example

--- chap08/ex8.py
+++ chap08/ex8.py
@@ -10,3 +10,12 @@ print formatter % (
 	"But it didn't sing.",
 	"So I said goodnight."
 )
+
+
+
+# chap08(master)[ 1 ] > python ex8.py
+# 1 2 3 4
+# 'one' 'two' 'three' 'four'
+# True False False True
+# '%r %r %r %r' '%r %r %r %r' '%r %r %r %r' '%r %r %r %r'
+# 'I had this thing.' 'That you could type up right.' "But it didn't sing." 'So I said goodnight.'
