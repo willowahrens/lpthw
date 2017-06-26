766b33e3de6177802438c79c1f95f78ed6a5bf9d
Willow Ahrens <willowahrens@Willows-MacBook-Pro.local>
2017-06-26 17:51:35 -0500

    example

--- ex9.py
+++ ex9.py
@@ -1,5 +1,5 @@
 days = "Mon Tue Wed Thu Fri Sat Sun"
-months = "Jan/nFeb/nMar/nApr/nMay/nJun/nJul/nAug"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
 print "Here are the days:", days
 print "Here are the months:", months
