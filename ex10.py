3ba77f5fc0902d9dcd90f362a5e3e6dd5101e6aa
Willow Ahrens <willowahrens@Willows-MacBook-Pro.local>
2017-06-26 17:59:51 -0500

    example

---
+++ ex10.py
@@ -0,0 +1,28 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backlash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* cat food
+\t* fishies
+\t* catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backlash_cat
+print fat_cat
+
+
+# lpthw(master) > python ex10.py
+# 	I'm tabbed in.
+# I'm split
+# on a line.
+# I'm \ a \ cat.
+
+# I'll do a list:
+# 	* cat food
+# 	* fishies
+# 	* catnip
+# 	* Grass
