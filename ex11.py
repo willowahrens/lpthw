45c136cef8ce06dffde4bbe25c6a02ce40abce0d
Willow Ahrens <willowahrens@Willows-MacBook-Pro.local>
2017-06-29 12:34:41 -0500

    example

---
+++ ex11.py
@@ -0,0 +1,15 @@
+print "How old are you?",
+age = raw_input()
+print "how tall are you?",
+height = raw_input()
+print "how much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (
+	age, height, weight
+
+# 	lpthw(master)[ 1 ] > python ex11.py
+# How old are you? 17
+# how tall are you? 5'8"
+# how much do you weigh? 136
+# So, you're '17' old, '5\'8"' tall and '136' heavy.
