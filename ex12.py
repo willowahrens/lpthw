1e803481ab13a7ec624d8bdc9a20b97df6f91c77
Willow Ahrens <willowahrens@Willows-MacBook-Pro.local>
2017-06-29 12:47:42 -0500

    example

---
+++ ex12.py
@@ -0,0 +1,6 @@
+age = raw_input("how old are you? ")
+height = raw_input("how tall are you? ")
+weight = raw_input("how much do you weigh? ")
+
+print "so you're %r old, %r tall and %r heavy." % (
+	age, height, weight)
