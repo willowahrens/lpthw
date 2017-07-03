664157a6fd156962d25eaec0aca1c33a8d60265f
Willow Ahrens <willowahrens@Willows-MacBook-Pro.local>
2017-07-03 12:43:05 -0500

    example

--- ex13.py
+++ ex13.py
@@ -7,7 +7,8 @@ print "Your first variable is:", first
 print "Your second variable is:", second
 print "Your third variable is:", third
 
-# lpthw(master)[ 1 ] > python ex13.py
-# Traceback (most recent call last):
-#   File "ex13.py", line 3, in <module>
-#     script, first, second, third = argv
+# lpthw(master)[ 1 ] > python ex13.py One Two Three
+# The script is called: ex13.py
+# Your first variable is: One
+# Your second variable is: Two
+# Your third variable is: Three
