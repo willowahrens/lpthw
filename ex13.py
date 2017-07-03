65ab8fa2656cc1c045153cd41043fd259797888c
Willow Ahrens <willowahrens@Willows-MacBook-Pro.local>
2017-07-03 12:25:01 -0500

    example

---
+++ ex13.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+# lpthw(master)[ 1 ] > python ex13.py
+# Traceback (most recent call last):
+#   File "ex13.py", line 3, in <module>
+#     script, first, second, third = argv
