b019aac3494411236419dfa53e17c864ad7f6167
Willow Ahrens <willowahrens@Willows-MacBook-Pro.local>
2017-07-03 12:44:03 -0500

    example

--- ex14.py
+++ ex14.py
@@ -19,3 +19,18 @@ Alright, so you said %r about liking me,
 You live in %r. Not sure where that is.
 And you have a %r computer. Nice.
 """ % (likes, lives, computer)
+
+
+# lpthw(master)[ 1 ] > python ex14.py willow
+# Hi willow, I'm the ex14.py script.
+# I'd like to ask you a few questions.
+# Do you like me willow?
+# > sure
+# Where do you live willow?
+# > fayetteville
+# What kind of computer do you have?
+# > mac
+
+# Alright, so you said 'sure' about liking me,
+# You live in 'fayetteville'. Not sure where that is.
+# And you have a 'mac' computer. Nice.
